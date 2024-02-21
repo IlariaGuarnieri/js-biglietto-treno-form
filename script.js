581e056c1fe404699fa95480e471a8b3a9ba1a06
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-02-21 16:57:32 +0100

    [delete] cancellazione esercizio precedente tenuto come riferimento

--- script.js
+++ script.js
@@ -9,22 +9,16 @@ nomeInvetato2 = 5;
 //inizializzazione = definizion + assegnamento (in una unica riga)
 const altroNomeInventato = "hello world"; 
 
+
 // ********** inizio **********
-
-const numKm ('Quanti km vuoi percorrere?');
-console.log (numKm);
-
-const etaPasseggero  ('Quanti anni hai? ');
-console.log (etaPasseggero);
 
 const prezzoPerKm = 0.21
 
-let prezzoTotale = numKm * prezzoPerKm
-console.log (prezzoTotale)
+
 
 const btn1 = document.querySelector('.btn1');
 
-btn7.addEventListener('click',function(){
+btn1.addEventListener('click',function(){
   // resetto il titolo
   titoloInput.innerHTML = testoTitoloInput;
   // resetto il value dell'input
@@ -32,14 +26,17 @@ btn7.addEventListener('click',function(){
 })
 
 
-if(etaPasseggero < 18){
-  message = 'Sei un passeggero minorenne, hai diritto ad uno sconto del 20% e il tuo biglietto costa: '
-  prezzoTotale = prezzoTotale - (prezzoTotale * 0.2)
-}else if (etaPasseggero > 65){
-  message = 'Sei un passeggero over 65, hai diritto ad uno sconto del 40% e il tuo biglietto costa: '
-  prezzoTotale = prezzoTotale - (prezzoTotale * 0.4)
-}else{
-  message = 'il tuo biglietto ha un costo di: '
-}
 
-document.getElementById('output').innerHTML = message + prezzoTotale.toFixed(2);
+
+
+// if(etaPasseggero < 18){
+//   message = 'Sei un passeggero minorenne, hai diritto ad uno sconto del 20% e il tuo biglietto costa: '
+//   prezzoTotale = prezzoTotale - (prezzoTotale * 0.2)
+// }else if (etaPasseggero > 65){
+//   message = 'Sei un passeggero over 65, hai diritto ad uno sconto del 40% e il tuo biglietto costa: '
+//   prezzoTotale = prezzoTotale - (prezzoTotale * 0.4)
+// }else{
+//   message = 'il tuo biglietto ha un costo di: '
+// }
+
+// document.getElementById('output').innerHTML = message + prezzoTotale.toFixed(2);
